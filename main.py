0bf562399671126cf6767d2122e27feeb904dd13
DaudiaD <matheo.bregier@mines-ales.org>
2023-02-17 15:33:53 +0100

    Cleaned and commented the code

--- main.py
+++ main.py
@@ -10,21 +10,15 @@ response = requests.get(url)
 # Parse the HTML content using BeautifulSoup
 soup = BeautifulSoup(response.content, 'html.parser')
 
-# Find all the album titles by searching for the relevant HTML tags and attributes
+# Find all the albums page url endings by searching for the relevant HTML tags and attributes
 album_titles = soup.find_all('a', {
     'class': 'Text__SCText-sc-kgt5u3-0 Link__SecondaryLink-sc-1vfcbn2-1 gwWwBt eDKWEX '
              'ProductListItem__StyledProductTitle-sc-ico7lo-3 ivaIVy'},
                              href=True)
 
-#href = album_titles[0]['href']
+# Creating a list containing all the albums page url
 href = []
 for i in range (len(album_titles)):
     href.append("https://www.senscritique.com"+album_titles[i]['href'])
-
-# Extract the text of the album titles and store them in a list
-#titles_list = [title.text for title in album_titles]
-
-# Print the list of album titles
-#print(titles_list[0])
 
 print(href)
